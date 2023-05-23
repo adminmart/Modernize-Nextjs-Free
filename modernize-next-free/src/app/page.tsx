77a0a1222f6113cf1f94384e3f8523c6d567206d
adminmart <info@adminmart.com>
2023-05-23 16:07:31 +0530

    ''

--- modernize-next-free/src/app/page.tsx
+++ modernize-next-free/src/app/page.tsx
@@ -1,9 +1,43 @@
+import { Grid, Box } from '@mui/material';
+import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
+// components
+import SalesOverview from '@/app/(DashboardLayout)/components/dashboard/SalesOverview';
+import YearlyBreakup from '@/app/(DashboardLayout)/components/dashboard/YearlyBreakup';
+import RecentTransactions from '@/app/(DashboardLayout)/components/dashboard/RecentTransactions';
+import ProductPerformance from '@/app/(DashboardLayout)/components/dashboard/ProductPerformance';
+import Blog from '@/app/(DashboardLayout)/components/dashboard/Blog';
+import MonthlyEarnings from '@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings';
 
 const DashLayout = () => {
   return (
-    <main>
-        This is the main pageadgag
-    </main>
+    <PageContainer title="Dashboard" description="this is Dashboard">
+      <Box>
+        <Grid container spacing={3}>
+          <Grid item xs={12} lg={8}>
+            <SalesOverview />
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <YearlyBreakup />
+              </Grid>
+              <Grid item xs={12}>
+                <MonthlyEarnings />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <RecentTransactions />
+          </Grid>
+          <Grid item xs={12} lg={8}>
+            <ProductPerformance />
+          </Grid>
+          <Grid item xs={12}>
+            <Blog />
+          </Grid>
+        </Grid>
+      </Box>
+    </PageContainer>
   )
 }
 
