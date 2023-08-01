50b8d794de4190724ea52e3d9dc8ba8841d0b275
niravjoshi <niravjoshi87@gmail.com>
2023-08-01 12:24:27 +0530

    ;;

--- package/next.config.js
+++ package/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { appDir: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
