ceda365aee6f1e1116b66cb6c86e10186474baf3
adminmart <info@adminmart.com>
2024-05-22 15:50:55 +0530

    updated

--- package/src/app/(DashboardLayout)/layout/header/Header.tsx
+++ package/src/app/(DashboardLayout)/layout/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button } from '@mui/material';
 import PropTypes from 'prop-types';
-
+import Link from 'next/link';
 // components
 import Profile from './Profile';
 import { IconBellRinging, IconMenu } from '@tabler/icons-react';
@@ -62,8 +62,8 @@ const Header = ({toggleMobileSidebar}: ItemType) => {
         </IconButton>
         <Box flexGrow={1} />
         <Stack spacing={1} direction="row" alignItems="center">
-          <Button variant="contained" disableElevation color="primary" >
-            Download
+          <Button variant="contained" component={Link} href="/authentication/login"   disableElevation color="primary" >
+            Login
           </Button>
           <Profile />
         </Stack>
--- package/src/app/(DashboardLayout)/layout/sidebar/Updrade.tsx
+++ package/src/app/(DashboardLayout)/layout/sidebar/Updrade.tsx
@@ -6,19 +6,19 @@ import Link from 'next/link';
 export const Upgrade = () => {
     return (
         <Box
-            display={'flex'}
+            display='flex'
             alignItems="center"
             gap={2}
-            sx={{ m: 3, p: 3, bgcolor: `${'primary.light'}`, borderRadius: '8px' }}
+            sx={{ m: 3, p: 3, bgcolor: 'primary.light', borderRadius: '8px' }}
         >
             <>
-                <Box>
-                    <Typography variant="h6" mb={1}>Unlimited Access</Typography>
-                    <Button color="primary" target="_blank" disableElevation component={Link} href="#" variant="contained" aria-label="logout" size="small">
-                        Upgrade
+                <Box >
+                    <Typography variant="h5" sx={{width:"80px"}} fontSize='16px' mb={1}>Haven`&apos;` account ?</Typography>
+                    <Button color="primary" target="_blank" disableElevation component={Link} href="/authentication/register" variant="contained" aria-label="logout" size="small">
+                        Sign Up
                     </Button>
                 </Box>
-                <Box mt="-35px">
+                <Box mt="-35px" >
                     <Image alt="Remy Sharp" src='/images/backgrounds/rocket.png' width={100} height={100} />
                 </Box>
             </>
